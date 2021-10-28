5719a9e55ca7be8367f69f3187a946734882fc97
Basay <Basay1980@gmail.com>
2021-10-28 14:54:37 +0300

    убрал один цикл

--- js/form.js
+++ js/form.js
@@ -1,10 +1,8 @@
 
 const TYPES = [['bungalow', 'Бунгало', 0], ['flat', 'Квартира', 1000], ['hotel', 'Отель', 3000],  ['house', 'Дом', 5000], ['palace', 'Дворец', 10000 ]];
 
-const rooms = [{'guestMax' : '1', 'rooms' : '1' },
-  {'guestMax' : '2', 'rooms' : '2'},
-  {'guestMax' : '3', 'rooms' : '3'},
-  {'guestMax' : '0', 'rooms' : '100'}];
+
+const roomsObj = {'1' : '1', '2' : '2', '3' : '3', '100' : '0'}; // rooms : max guests
 
 
 const form = document.querySelector('.notice');
@@ -26,18 +24,14 @@ const setDisabledGuests = (key) => {
       guest.setAttribute('disabled', 'disabled');
     });
   }
-  rooms.forEach( (obj)=> {
-    if ( obj.rooms === key){
-      guestOptions.forEach((guest) => {
-        if (guest.value < obj.guestMax && guest.value !== '0' ) {
-          guest.removeAttribute('disabled');
-        }
-      });
-    }
-    if (obj.rooms === key && obj.guestMax === '0'){
-      guests.querySelector('option[value="0"]').removeAttribute('disabled');
+  guestOptions.forEach((guest) => {
+    if (guest.value < roomsObj[key] && guest.value !== '0' ) {
+      guest.removeAttribute('disabled');
     }
   });
+  if (roomsObj[key] === '0'){
+    guests.querySelector('option[value="0"]').removeAttribute('disabled');
+  }
 };
 
 
